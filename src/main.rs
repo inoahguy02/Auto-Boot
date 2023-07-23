17657218ab33e79d4ff543ef95383f78c0eaded1
Noah <noahgarland02@gmail.com>
2023-07-22 23:20:40 -0500

    Added first command and added a temporary fix for cmd requiring admin rights. Will prob need to change that

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-mod output_str;
+use std::process::Command;
 
 fn main() {
-    output_str::printstr("Hello");
+    /*Not going to implement auto usb plug in listener just yet
+    Will go ahead and implement Windows -> Linux using cmd
+    Probably ask user for distro
+
+    Run cmd as admin
+    Use command: bcdedit /enum firmware
+    Search for OS (e.x. Ubuntu) and grab identifier string
+    Use command: bcdedit /set {fwbootmgr} default {identifier} (Replace identifier with grabbed string)
+    Use command: shutdown /r /t 0 (Make sure user knows that the system will be shut down and to save everything)
+    Should boot straight in to OS*/
+
+    //For windows. Change to sh for linux
+    let output = if cfg!(target_os = "windows")
+    {
+        Command::new("cmd")
+            .args(["/C", "bcdedit /enum firmware"])
+            .output()
+            .expect("Failed to execute")
+    }
+    else
+    {
+        return;
+    };
+    
+    println!("Output: {}", String::from_utf8_lossy(&output.stdout));
+    std::io::stdin().read_line(&mut String::new()).unwrap();
 }
--- src/output_str.rs
+++
@@ -1,4 +0,0 @@
-pub fn printstr(str_to_print: &str)
-{
-    println!("{}", str_to_print);
-}
