3a014b8b786f2bd41b2d09bd15835fa509efb43b
Noah <noahgarland02@gmail.com>
2023-07-23 23:09:30 -0500

    Added linux to windows support. Should work for all distros of linux

--- src/main.rs
+++ src/main.rs
@@ -16,49 +16,92 @@ fn main() {
     //For windows. Change to sh for linux
     
     //Get GUID for target OS
-    let output = if cfg!(target_os = "windows")
+    if cfg!(target_os = "windows")
     {
-        Command::new("cmd")
+        let output = Command::new("cmd")
             .arg("/C bcdedit /enum firmware")
             .output()
-            .expect("Failed to execute")
+            .expect("Failed to execute");
+
+        //Use regex to parse OS's section, then parse the GUID
+        //Rust doesn't support lookarounds in its Regex which is dumb
+        let con_output = String::from_utf8_lossy(&output.stdout).to_string();
+        let section = Regex::new(r"identifier.+(?:\n.+){3}ubuntu").unwrap();
+        let guid;
+
+        if let Some(capture) = section.captures(&con_output)
+        {
+            let guid_pattern = Regex::new(r"\{\S+\}").unwrap();
+            guid = guid_pattern.find(capture.get(0).unwrap().as_str()).unwrap().as_str();
+            println!("Capture: {}", guid);
+        }
+        else
+        {
+            println!("No capture found");
+            return;
+        };
+
+        //Use GUID to set Ubuntu as default boot
+        Command::new("cmd")
+            .arg(format!("/C bcdedit /set {{fwbootmgr}} default {}", guid))
+            .output()
+            .expect("Failed to execute");
+        
+        //Restart machine
+        Command::new("cmd")
+            .arg("/C shutdown /r /t 0")
+            .output()
+            .expect("Failed to execute");
+
+        //Keep app running
+        //std::io::stdin().read_line(&mut String::new()).unwrap();
     }
+    else if cfg!(target_os = "linux")
+    {
+        //This will ask user for password
+        let output = Command::new("sudo")
+            .arg("efibootmgr")
+            .output()
+            .expect("Failed to execute");
+
+        let con_output = String::from_utf8_lossy(&output.stdout).to_string();
+        let line = Regex::new(r"\d{4}\* Windows").unwrap();
+        let boot_num;
+
+        if let Some(capture) = line.captures(&con_output)
+        {
+            let guid_pattern = Regex::new(r"\d{4}").unwrap();
+            boot_num = guid_pattern.find(capture.get(0).unwrap().as_str()).unwrap().as_str();
+            println!("Capture: {}", boot_num);
+        }
+        else
+        {
+            println!("No capture found: {}", String::from_utf8_lossy(&output.stderr).to_string());
+            return;
+        };
+
+        //Set Windows as default boot
+        Command::new("sudo")
+            .args(["efibootmgr", "-o", boot_num])
+            .output()
+            .expect("Failed to execute");
+        
+        //Restart machine
+        Command::new("sudo")
+            .arg("reboot")
+            .output()
+            .expect("Failed to execute");
+    }
+    /*else if cfg!(target_os = "macOS")
+    {
+        //Don't have mac so idk when this will be a thing
+        //Can prob find a cheap one on ebay or something idk
+    }*/
     else
     {
-        //Not supported yet
+        //Can't find OS
         return;
-    };
-    
-    //Use regex to parse OS's section, then parse the GUID
-    //Rust doesn't support lookarounds in its Regex which is dumb
-    let con_output = String::from_utf8_lossy(&output.stdout).to_string();
-    let section = Regex::new(r"identifier.+(?:\n.+){3}ubuntu").unwrap();
-    let guid;
-
-    if let Some(capture) = section.captures(&con_output)
-    {
-        let guid_pattern = Regex::new(r"\{\S+\}").unwrap();
-        guid = guid_pattern.find(capture.get(0).unwrap().as_str()).unwrap().as_str();
-        println!("Capture: {}", guid);
     }
-    else
-    {
-        println!("No capture found");
-        return;
-    };
-
-    //Use GUID to set OS as default boot
-    Command::new("cmd")
-        .arg(format!("/C bcdedit /set {{fwbootmgr}} default {}", guid))
-        .output()
-        .expect("Failed to execute");
     
-    //Restart machine
-    Command::new("cmd")
-        .arg("/C shutdown /r /t 0")
-        .output()
-        .expect("Failed to execute");
-
-    //Keep app running
-    //std::io::stdin().read_line(&mut String::new()).unwrap();
+    
 }
